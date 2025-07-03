86100e7dd1c75cf0fe9914a2fe889a7723effb5d
Yeshu4545 <yashwanthyeshu27@gmail.com>
2025-07-03 20:16:57 +0530

    Fix: Use MONGO_URI from env for DB connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/mydatabase", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     })
